200a5544aed8fc3f9e0b7349d106b546230c5e23
irbyk <irb.byk@gmail.com>
2018-01-26 01:16:38 +0100

    prout

---
+++ projet/src/Monstre.java
@@ -0,0 +1,18 @@
+
+public class Monstre {
+	float force;
+	float dexterite;
+	float constitution;
+	float intelligence;
+	float sagesse;
+	float charisme;
+	public Monstre(float force, float dex, float cons, float intel, float sag, float chari) {
+		this.force = force;
+		this.dexterite = dex;
+		this.constitution = cons;
+		this.intelligence = intel;
+		this.sagesse = sag;
+		this.charisme = chari;
+	}
+
+}
